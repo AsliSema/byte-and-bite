b72614a1e7a828b1ca7e7764395ca5a2b65f0f5b
Asli Sema Gultekin <aslisemagultekin@gmail.com>
2023-07-19 19:31:11 +0300

    delete dish inside cart

--- src/routes/cart.ts
+++ src/routes/cart.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { allowedTo, protect } from '../middlewares/authMiddleware';
-import { addDishToCart } from '../controllers/cart';
+import { addDishToCart, deleteDishFromCartItems } from '../controllers/cart';
 
 const router = express.Router();
 
 router.route("/").post(protect,allowedTo(['customer']), addDishToCart);
+router.route("/:dishID").delete(protect, allowedTo(['customer']), deleteDishFromCartItems);
 
 export default router;
