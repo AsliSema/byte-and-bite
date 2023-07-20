0d88b7ee1d2c628da6fd119484510cf065cc602a
Khaled6120 <khalednaes3@gmail.com>
2023-07-20 16:53:38 +0300

    ADD 3 routes ADD 3 controllers

--- src/routes/cart.ts
+++ src/routes/cart.ts
@@ -1,10 +1,24 @@
 import express from 'express';
 import { allowedTo, protect } from '../middlewares/authMiddleware';
-import { addDishToCart, deleteDishFromCartItems } from '../controllers/cart';
+import {
+  addDishToCart,
+  clearCart,
+  deleteDishFromCartItems,
+  getLoggedUserCart,
+  updateCartItemQuantity,
+} from '../controllers/cart';
 
 const router = express.Router();
 
-router.route("/").post(protect,allowedTo(['customer']), addDishToCart);
-router.route("/:dishID").delete(protect, allowedTo(['customer']), deleteDishFromCartItems);
+router
+  .route('/')
+  .get(protect, allowedTo(['customer']), getLoggedUserCart)
+  .post(protect, allowedTo(['customer']), addDishToCart)
+  .delete(protect, allowedTo(['customer']), clearCart);
+
+router
+  .route('/:dishID')
+  .delete(protect, allowedTo(['customer']), deleteDishFromCartItems)
+  .put(protect, allowedTo(['customer']), updateCartItemQuantity);
 
 export default router;
