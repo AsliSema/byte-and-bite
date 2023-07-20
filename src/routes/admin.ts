90dbf7fbc4f1404e8bb4ad28e82afc07e92b05d5
Asli Sema Gultekin <aslisemagultekin@gmail.com>
2023-07-20 14:06:16 +0300

    create new dish for admin

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import { updateUser } from '../controllers/admin';
+import { createDish } from '../controllers/dish';
 import { allowedTo, protect } from '../middlewares/authMiddleware';
 
 
 const router = express.Router();
 
 router.route('/users/:userID').put(protect, allowedTo(['admin']), updateUser);
+router.route("/dishes").post(protect, allowedTo(['admin']), createDish);
 
 
 export default router;
