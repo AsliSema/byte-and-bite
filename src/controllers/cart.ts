ce0a3560ce1f26f653bbcfcf180fd5800ed5b1d4
Khaled6120 <khalednaes3@gmail.com>
2023-07-20 10:22:28 +0300

    ADD updateCartItemQuantity + clearCart controllers ADD DELETE /api/cart + PUT /api/cart/:dishID

--- src/controllers/cart.ts
+++ src/controllers/cart.ts
@@ -1,26 +1,28 @@
-import { NextFunction, Request, Response } from "../types/express";
-import { StatusCodes } from "http-status-codes";
-import asyncHandler from "express-async-handler";
-import { ApiError } from "../utils/apiError";
-import Cart from "../models/cart";
-import Dish from "../models/dish";
-
+import { NextFunction, Request, Response } from '../types/express';
+import { StatusCodes } from 'http-status-codes';
+import asyncHandler from 'express-async-handler';
+import { ApiError } from '../utils/apiError';
+import Cart from '../models/cart';
+import Dish from '../models/dish';
 
 export enum qtyState {
-    less_than_zero = 1,
-    reqQty_bigger = 2,
-    availableToAdd = 3
+  less_than_zero = 1,
+  reqQty_bigger = 2,
+  availableToAdd = 3,
 }
 
-const checkQuantity = (requestedQuantity: number, availableQuantity: number) => {
-    if (requestedQuantity <= 0) {
-        return qtyState.less_than_zero;
-    }
-    if(requestedQuantity > availableQuantity){
-        return qtyState.reqQty_bigger;
-    }
-    return qtyState.availableToAdd;
-}
+const checkQuantity = (
+  requestedQuantity: number,
+  availableQuantity: number
+) => {
+  if (requestedQuantity <= 0) {
+    return qtyState.less_than_zero;
+  }
+  if (requestedQuantity > availableQuantity) {
+    return qtyState.reqQty_bigger;
+  }
+  return qtyState.availableToAdd;
+};
 
 // const calculateTotalInCart = (cart: ,dish) => {
 //     let totalPrice = 0
@@ -30,69 +32,159 @@ const checkQuantity = (requestedQuantity: number, availableQuantity: number) => 
 //     cart.totalCartPrice = totalPrice;
 // }
 
-const addDishToCart = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    const { dishID, quantity } = req.body
+const addDishToCart = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { dishID, quantity } = req.body;
 
-    const dish = await Dish.findById(dishID)
+    const dish = await Dish.findById(dishID);
 
-    let cart = await Cart.findOne({user: req.user?._id})
+    let cart = await Cart.findOne({ user: req.user?._id });
 
-    if(!dish){
-        return next(new ApiError(StatusCodes.BAD_REQUEST, `Dish not found!`))
+    if (!dish) {
+      return next(new ApiError(StatusCodes.BAD_REQUEST, `Dish not found!`));
     }
     if (checkQuantity(quantity, dish.quantity) === qtyState.less_than_zero) {
-        return next(new ApiError(StatusCodes.BAD_REQUEST, 'Quantity is not available for this dish!'));
+      return next(
+        new ApiError(
+          StatusCodes.BAD_REQUEST,
+          'Quantity is not available for this dish!'
+        )
+      );
     }
-    
-    // no cart yet
-    if(!cart){
-        if(checkQuantity(quantity, dish.quantity) === qtyState.reqQty_bigger) {
-            return next(new ApiError(StatusCodes.BAD_REQUEST, 'Quantity is not available for this dish!'));
-        }
-       cart = await Cart.create({
-            user: req.user?._id,
-            cartItems: [
-                {
-                    product: dishID,
-                    quantity
 
-                }
-            ],
-            cookID: dish.cook
-        })
+    // no cart yet
+    if (!cart) {
+      if (checkQuantity(quantity, dish.quantity) === qtyState.reqQty_bigger) {
+        return next(
+          new ApiError(
+            StatusCodes.BAD_REQUEST,
+            'Quantity is not available for this dish!'
+          )
+        );
+      }
+      cart = await Cart.create({
+        user: req.user?._id,
+        cartItems: [
+          {
+            product: dishID,
+            quantity,
+          },
+        ],
+        cookID: dish.cook,
+      });
     }
     // cart exists
-    else{
-        // check if dish exists in cart
-        const dishInCart = cart?.cartItems.find(item => item.product == dishID);
-        if (dishInCart) {
-            // quantity = checkQuantity()
-            if (checkQuantity(dishInCart.quantity + quantity, dish.quantity ) === qtyState.reqQty_bigger) {
-                // return next(new ApiError(StatusCodes.NOT_ACCEPTABLE, `Quantity is not available for this dish!`))
-                dishInCart.quantity = dish.quantity;
-            }
-            else {
-                dishInCart.quantity += quantity;
-            }
+    else {
+      // check if dish exists in cart
+      const dishInCart = cart?.cartItems.find((item) => item.product == dishID);
+      if (dishInCart) {
+        // quantity = checkQuantity()
+        if (
+          checkQuantity(dishInCart.quantity + quantity, dish.quantity) ===
+          qtyState.reqQty_bigger
+        ) {
+          // return next(new ApiError(StatusCodes.NOT_ACCEPTABLE, `Quantity is not available for this dish!`))
+          dishInCart.quantity = dish.quantity;
+        } else {
+          dishInCart.quantity += quantity;
         }
-        else if (cart.cookID === dish.cook.toString()) {
-            cart.cartItems.push({ product: dishID, quantity})
-        }else{
-            return next(new ApiError(StatusCodes.NOT_ACCEPTABLE, `You can not add the dishes from different cooks `))
-        }
+      } else if (cart.cookID === dish.cook.toString()) {
+        cart.cartItems.push({ product: dishID, quantity });
+      } else {
+        return next(
+          new ApiError(
+            StatusCodes.NOT_ACCEPTABLE,
+            `You can not add the dishes from different cooks `
+          )
+        );
+      }
     }
-    let totalPrice = 0
-    cart.cartItems.forEach(item => {
-        totalPrice += (item.quantity*dish.price);
-    })
+    let totalPrice = 0;
+    cart.cartItems.forEach((item) => {
+      totalPrice += item.quantity * dish.price;
+    });
     cart.totalCartPrice = totalPrice;
-    
-    await cart.save()
-    res.status(StatusCodes.OK).json({
-        data: cart
-    })
-})
 
-export {
-    addDishToCart
-}
+    await cart.save();
+    res.status(StatusCodes.OK).json({
+      data: cart,
+    });
+  }
+);
+
+// @desc       Clear logged user cart
+// @route      DELETE   /api/cart
+// @access     private/customer
+
+const clearCart = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    await Cart.findOneAndDelete({ user: req.user?._id });
+
+    res.status(StatusCodes.NO_CONTENT).send();
+  }
+);
+
+// @desc    Update specific cart item quantity
+// @route   PUT /api/cart/:dishID
+// @access  Private/Customer
+const updateCartItemQuantity = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { quantity } = req.body;
+    const dish = await Dish.findById(req.params.dishID);
+
+    if (!dish)
+      return next(new ApiError(StatusCodes.BAD_REQUEST, `Dish not found!`));
+
+    const maxQuantity = dish.quantity;
+
+    const cart = await Cart.findOne({ user: req?.user?._id });
+    if (!cart) {
+      return next(
+        new ApiError(
+          StatusCodes.NOT_FOUND,
+          `There is no cart for user: ${req?.user?._id}`
+        )
+      );
+    }
+
+    const itemIndex = cart.cartItems.findIndex(
+      (item) => item.product.toString() === req.params.dishID
+    );
+    if (itemIndex > -1 && quantity > 0 && quantity <= maxQuantity) {
+      const cartItem = cart.cartItems[itemIndex];
+      cartItem.quantity = quantity;
+      cart.cartItems[itemIndex] = cartItem;
+    } else if (quantity <= 0 || quantity > maxQuantity) {
+      return next(
+        new ApiError(
+          StatusCodes.NOT_FOUND,
+          `Quantity must be more than 0 and less than ${maxQuantity}, please provide a valid value.`
+        )
+      );
+    } else {
+      return next(
+        new ApiError(
+          StatusCodes.NOT_FOUND,
+          `There is no dish for this id: ${req.params.itemId}`
+        )
+      );
+    }
+
+    let totalPrice = 0;
+    cart.cartItems.forEach((item) => {
+      totalPrice += item.quantity * dish.price;
+    });
+    cart.totalCartPrice = totalPrice;
+
+    cart.totalCartPrice = totalPrice;
+
+    await cart.save();
+
+    res.status(StatusCodes.OK).json({
+      numOfCartItems: cart.cartItems.length,
+      data: cart,
+    });
+  }
+);
+
+export { addDishToCart, clearCart, updateCartItemQuantity };
--- src/routes/cart.ts
+++ src/routes/cart.ts
@@ -1,9 +1,20 @@
 import express from 'express';
 import { allowedTo, protect } from '../middlewares/authMiddleware';
-import { addDishToCart } from '../controllers/cart';
+import {
+  addDishToCart,
+  clearCart,
+  updateCartItemQuantity,
+} from '../controllers/cart';
 
 const router = express.Router();
 
-router.route("/").post(protect,allowedTo(['customer']), addDishToCart);
+router
+  .route('/')
+  .post(protect, allowedTo(['customer']), addDishToCart)
+  .delete(protect, allowedTo(['customer']), clearCart);
+
+router
+  .route('/:dishID')
+  .put(protect, allowedTo(['customer']), updateCartItemQuantity);
 
 export default router;
