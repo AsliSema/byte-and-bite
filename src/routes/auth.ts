e5f95cbd22072bc15128acf066e30343eb755fc6
Ahmad Ramin Soleyman kheyl <sramin963@gmail.com>
2023-07-28 20:34:43 +0300

    ramin swagger task status code in auth middleware

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -12,43 +12,7 @@ router.route('/signup').post(signupValidator, registerUser);
 router.route('/signin').post(signinValidator, signinUser);
 router.route('/profile').get(protect, getUserProfile);
 
-/**
- * @swagger
- * /api/users/:
- *   get:
- *     summary: Get all users (admins only)
- *     description: Get a list of all users. This route is accessible to admins only.
- *     tags:
- *       - Users
- *     security:
- *       - bearerAuth: []
- *     responses:
- *       200:
- *         description: Successful response with the list of users.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 Users:
- *                   type: array
- *                   items:
- *                     $ref: '#/components/schemas/User'
- *                 page:
- *                   type: number
- *                   description: The current page number.
- *                 pages:
- *                   type: number
- *                   description: The total number of pages.
- *       401:
- *         $ref: '#/components/responses/UnauthorizedError'
- *       403:
- *         $ref: '#/components/responses/ForbiddenError'
- *       500:
- *         $ref: '#/components/responses/InternalServerError'
- */
 
-router.route("/").get(protect, allowedTo(["admin"]), getAllUsers);
 
 /**
  * @swagger
@@ -77,20 +41,10 @@ router.route("/").get(protect, allowedTo(["admin"]), getAllUsers);
  *     responses:
  *       200:
  *         description: Successful response with the updated user data.
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/User'
- *       400:
- *         $ref: '#/components/responses/BadRequestError'
  *       401:
- *         $ref: '#/components/responses/UnauthorizedError'
+ *         description: Unauthorized
  *       403:
- *         $ref: '#/components/responses/ForbiddenError'
- *       404:
- *         $ref: '#/components/responses/NotFoundError'
- *       500:
- *         $ref: '#/components/responses/InternalServerError'
+ *         description: Forbidden 
  */
 
 router.route("/:userID").put(protect, allowedTo(["customer", "cook"]), updateUser);
--- src/routes/cart.ts
+++ src/routes/cart.ts
@@ -17,31 +17,15 @@ router
  * @swagger
  * /api/cart:
  *   get:
- *     summary: Get Logged user cart
- *     description: Retrieve the logged-in user's cart.
+ *     summary: Get user cart
+ *     description: Retrieve the user's cart.
  *     tags:
  *       - Cart
- *     security:
- *       - bearerAuth: []
  *     responses:
  *       '200':
  *         description: Successful response with cart information.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 numOfCartItems:
- *                   type: integer
- *                   description: Number of items in the cart.
- *                 data:
- *                   $ref: '#/components/schemas/Cart'
  *       '404':
  *         description: Cart not found for the user.
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/Error'
  */
   .get(protect, allowedTo(['customer']), getLoggedUserCart)
 
@@ -53,50 +37,23 @@ router
  *     description: Add a dish to the cart items of the logged-in user. If the cart does not exist, it will be created.
  *     tags:
  *       - Cart
- *     security:
- *       - bearerAuth: []
  *     requestBody:
  *       required: true
  *       content:
  *         application/json:
  *           schema:
- *             type: object
- *             properties:
- *               dishID:
- *                 type: string
- *                 description: ID of the dish to be added to the cart.
- *               quantity:
- *                 type: integer
- *                 description: Quantity of the dish to be added to the cart.
- *                 minimum: 1
+ *             $ref: '#/components/schemas/Dish'
  *     responses:
  *       '200':
  *         description: Successful response with updated cart information.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 data:
- *                   $ref: '#/components/schemas/Cart'
  *       '400':
  *         description: Dish not found or invalid quantity.
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/Error'
+ *       '401':
+ *         description: UNAUTHORIZED.
+ *       '403':
+ *         description: FORBIDDEN.
  *       '406':
- *         description: Dish quantity not available for this dish.
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/Error'
- *       '422':
- *         description: Unable to add the dish to the cart.
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/Error'
+ *         description: NOT_ACCEPTABLE if the user added a dish from diffrent cook.
  */
 
   .post(protect, allowedTo(['customer']), addDishToCart)
@@ -105,21 +62,17 @@ router
  * @swagger
  * /api/cart:
  *   delete:
- *     summary: Clear logged user cart
- *     description: Clear the cart items of the logged-in user.
+ *     summary: Clear user cart
+ *     description: Clear the cart items of the user.
  *     tags:
  *       - Cart
- *     security:
- *       - bearerAuth: []
  *     responses:
  *       '204':
- *         description: Cart cleared successfully.
- *       '404':
- *         description: Cart not found for the user.
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/Error'
+ *         description: Cart deleted successfully.
+ *       '401':
+ *         description: UNAUTHORIZED.
+ *       '403':
+ *         description: FORBIDDEN.
  */
   .delete(protect, allowedTo(['customer']), clearCart);
 
@@ -134,31 +87,22 @@ router
  *     description: Delete a specific dish from the cart items of the logged-in user.
  *     tags:
  *       - Cart
- *     security:
- *       - bearerAuth: []
  *     parameters:
  *       - in: path
  *         name: dishID
  *         required: true
  *         schema:
  *           type: string
- *         description: ID of the dish to be deleted from the cart.
+ *           description: ID of the dish to be deleted from the cart.
  *     responses:
  *       '200':
  *         description: Successful response with updated cart information.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 data:
- *                   $ref: '#/components/schemas/Cart'
  *       '400':
- *         description: Dish not found in the cart.
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/Error'
+ *         description: Dish or Cart not found.
+ *       '401':
+ *         description: UNAUTHORIZED.
+ *       '403':
+ *         description: FORBIDDEN.
  */
   .delete(protect, allowedTo(['customer']), deleteDishFromCartItems)
   .put(protect, allowedTo(['customer']), updateCartItemQuantity);
