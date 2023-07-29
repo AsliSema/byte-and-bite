e825ed72c9507e4f84fcce23c5cc37ecfec61a88
baraah-berra <baraahalsaidramadan@gmail.com>
2023-07-29 12:53:22 +0300

    add swagger to admin.ts and auth.ts
    Add Swagger for those routes:
    /orders GET (admin.ts route).
    /order/:orderID GET (admin.ts route).
    /order/:orderID PUT (admin.ts route).
    /signup POST (auth.ts route).
    /signin POST (auth.ts route).
    /profile GET (auth.ts route).

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -8,9 +8,105 @@ const {
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Auth
+ *   description: API endpoints for user authentication and users
+ */
+
+/**
+ * @swagger
+ * /signup:
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *             required:
+ *               - name
+ *               - email
+ *               - password
+ *     responses:
+ *       200:
+ *         description: Successful registration
+ *       400:
+ *         description: Bad request
+ */
+
 router.route('/signup').post(signupValidator, registerUser);
 router.route('/signin').post(signinValidator, signinUser);
+
+/**
+ * @swagger
+ * /signin:
+ *   post:
+ *     summary: User login
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *             required:
+ *               - email
+ *               - password
+ *     responses:
+ *       200:
+ *         description: Successful login
+ *       400:
+ *         description: Bad request
+ *       401:
+ *         description: Unauthorized
+ */
+router.post('/signin', signinValidator, signinUser);
+
+/**
+ * @swagger
+ * /profile:
+ *   get:
+ *     summary: Get user profile
+ *     tags: [Auth]
+ *     responses:
+ *       200:
+ *         description: Successful operation
+ *       401:
+ *         description: Unauthorized
+ */
+
 router.route('/profile').get(protect, getUserProfile);
+
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: Get all users (admin only)
+ *     tags: [Auth]
+ *     responses:
+ *       200:
+ *         description: Successful operation
+ *       401:
+ *         description: Unauthorized
+ *       403:
+ *         description: Forbidden
+ */
 router.route("/").get(protect, allowedTo(["admin"]), getAllUsers);
 router.route("/:userID").put(protect, allowedTo(["customer", "cook"]), updateUser);
 
