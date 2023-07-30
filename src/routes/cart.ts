b84d4cbab09cc50e2ba2fac6733e1b79d143a464
Nour Krimesh <nour.krimesh@gmail.com>
2023-07-30 20:39:05 +0300

    finished Swagger task 4

--- src/routes/cart.ts
+++ src/routes/cart.ts
@@ -105,6 +105,44 @@ router
  *         description: FORBIDDEN.
  */
   .delete(protect, allowedTo(['customer']), deleteDishFromCartItems)
+
+  /**
+   * @openapi
+   * /api/cart/{dishID}:
+   *   put:
+   *     summary: Update cart item quantity
+   *     description: Update dish quantity in cart.
+   *     tags:
+   *       - Cart
+   *     parameters:
+   *       - name: dishID
+   *         in: path
+   *         required: true
+   *         default: 64b422e9cd4f0261704f3f38
+   *         description: The ID of the dish to be updated
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             properties:
+   *               quantity:
+   *                  type: number
+   *                  default: 1
+   *                  description: in-cart dish quantity
+   *     responses:
+   *       '200':
+   *         description: Successful response with updated cart information.
+   *       '400':
+   *         description: BAD_REQUEST Dish not found or wrong dishID value.
+   *       '401':
+   *         description: UNAUTHORIZED.
+   *       '403':
+   *         description: FORBIDDEN.
+   *       '404':
+   *         description: NOT_FOUND if the user doesn't have a cart yet.
+   */
   .put(protect, allowedTo(['customer']), updateCartItemQuantity);
 
 export default router;
--- src/routes/dish.ts
+++ src/routes/dish.ts
@@ -22,21 +22,153 @@ import {
 
 const router = express.Router();
 
-router
-  .route('/')
+router.route('/')
+
+  /**
+ * @openapi
+ * /api/dish/:
+ *   get:
+ *     summary: Get All Dishes
+ *     description: Retrieve dishes which related to the customer's location also the customer can filter by dish category<br>Retrieve dishes which related to the cook's ID<br>fetches just 10 dish if the user not logged in.
+ *     tags:
+ *       - Dish
+ *     parameters:
+ *       - name: category
+ *         type: number
+ *         in: query
+ *         required: false
+ *         description: To filter dishes by their category
+ *       - name: pageSize
+ *         in: query
+ *         type: number
+ *         required: false
+ *         description: Results count to be fetched in one page
+ *       - name: pageNumber
+ *         type: number
+ *         in: query
+ *         required: false
+ *         description: The dish page that needs to be fetched
+ *     responses:
+ *       '200':
+ *         description: Successful response with an array of dishes and empty array of dishes if nothing found.
+ */
   .get(getUser, getAllDishes)
+
+  /**
+   * @openapi
+   * /api/dish:
+   *   post:
+   *     summary: Create a dish
+   *     description: Create a dish (Cook Only)
+   *     tags:
+   *       - Dish
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             $ref: '#/components/schemas/Dish'
+   *     responses:
+   *       '200':
+   *         description: Successful response with created dish information.
+   *       '400':
+   *         description: BAD_REQUEST for invalid dish data.
+   *       '401':
+   *         description: UNAUTHORIZED if the user is not logged in or his token is invalid.
+   *       '403':
+   *         description: FORBIDDEN. if the user is not a cook
+   */
   .post(protect, allowedTo(['cook']), createDishValidator, createDish);
 
 router
   .route('/:id')
+
+  /**
+ * @openapi
+ * /api/dish/{dishID}:
+ *   get:
+ *     summary: Get a dish by ID
+ *     description: Getting dish's info by it's ID
+ *     tags:
+ *       - Dish
+ *     parameters:
+ *       - name: dishID
+ *         type: string
+ *         in: path
+ *         required: true
+ *         defaul: 64b422e9cd4f0261704f3f38
+ *         description: Dish ID to be fetched
+ *     responses:
+ *       '200':
+ *         description: Successful response with an array of dishes and empty array of dishes if nothing found.
+ *       '400':
+ *         description: BAD_REQUEST if it's invalid ID
+ *       '404':
+ *         description: NOT_FOUND if the dish not found
+ */
   .get(getDishValidator, getDishById)
+
+  /**
+   * @openapi
+   * /api/dish/{dishID}:
+   *   put:
+   *     summary: Update Dish Data
+   *     description: Update dish data (Cook Only).
+   *     tags:
+   *       - Dish
+   *     parameters:
+   *       - name: dishID
+   *         in: path
+   *         required: true
+   *         default: 64b422e9cd4f0261704f3f38
+   *         description: The ID of the dish to be updated
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             $ref: '#/components/schemas/Dish'
+   *     responses:
+   *       '200':
+   *         description: Successful response with updated dish information.
+   *       '400':
+   *         description: BAD_REQUEST Dish not found or wrong dishID value.
+   *       '401':
+   *         description: UNAUTHORIZED if the user is not logged in or his token is invalid.
+   *       '403':
+   *         description: FORBIDDEN. if user is not a cook
+   *       '404':
+   *         description: NOT_FOUND if the dish not found.
+   */
   .put(protect, allowedTo(['cook']), updateDishValidator, updateDish)
-  .delete(
-    protect,
-    allowedTo(['admin', 'cook']),
-    deleteDishValidator,
-    deleteDish
-  );
+
+  /**
+ * @openapi
+ * /api/dish/{dishID}:
+ *   delete:
+ *     summary: Delete a dish by ID
+ *     description: Delete a dish by it's ID
+ *     tags:
+ *       - Dish
+ *     parameters:
+ *       - name: dishID
+ *         type: string
+ *         in: path
+ *         required: true
+ *         description: Dish ID to be deleted
+ *     responses:
+ *       '200':
+ *         description: Successful response with the deleted dish info.
+ *       '400':
+ *         description: BAD_REQUEST if it's invalid ID.
+ *       '401':
+ *         description: UNAUTHORIZED if the user is not logged in or his token is invalid.
+ *       '403':
+ *         description: FORBIDDEN if the user is not an admin or cook who own this dish
+ *       '404':
+ *         description: NOT_FOUND if the dish not found
+ */
+  .delete(protect, allowedTo(['admin', 'cook']), deleteDishValidator, deleteDish);
 
 /**
  * @openapi
