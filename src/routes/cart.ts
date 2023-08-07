4b33d902da3ad33bb4cd2acc904b2e8ac4c49fbb
Ahmad Ramin Soleyman kheyl <sramin963@gmail.com>
2023-08-07 12:24:32 +0300

    Update the name clearCart to deleteCart from ./controllers/cart and ./routes/cart

--- src/routes/cart.ts
+++ src/routes/cart.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { allowedTo, protect } from '../middlewares/authMiddleware';
 import {
   addDishToCart,
-  clearCart,
+  deleteCart,
   deleteDishFromCartItems,
   getLoggedUserCart,
   updateCartItemQuantity,
@@ -62,8 +62,8 @@ router
  * @swagger
  * /api/cart:
  *   delete:
- *     summary: Clear user cart
- *     description: Clear the cart items of the user.
+ *     summary: Delete user cart
+ *     description: Delete the cart items of the user.
  *     tags:
  *       - Cart
  *     responses:
@@ -74,7 +74,7 @@ router
  *       '403':
  *         description: FORBIDDEN.
  */
-  .delete(protect, allowedTo(['customer']), clearCart);
+  .delete(protect, allowedTo(['customer']), deleteCart);
 
 router
   .route('/:dishID')
