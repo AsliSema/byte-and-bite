91d826535c56c2498c08d717ae8ee3739aecb5fe
Nour Krimesh <nour.krimesh@gmail.com>
2023-08-09 01:24:23 +0300

    update cart route tests to clear everything after

--- src/routes/__tests__/cart.test.ts
+++ src/routes/__tests__/cart.test.ts
@@ -2,22 +2,83 @@ import request from 'supertest';
 import app from '../../app';
 import { config } from '../../config/config';
 import { StatusCodes } from 'http-status-codes';
+import User from '../../models/user';
+import generateToken from '../../utils/generateToken';
+import Dish from '../../models/dish';
 
 describe('Cart routes', () => {
 
-    const token = config.testCustomer.token;
-    const dishID = config.testDish.id;
+    let customerUser;
+    let customerToken;
+    let cookUser;
+    let cookToken;
+    let testDish;
+
+    beforeAll(async () => {
+        // create a test customer
+        customerUser = await User.create({
+            firstname: 'Customer',
+            lastname: 'Cart',
+            email: 'customer@cart.test',
+            password: 'testpassword',
+            phone: '5554567890',
+            address: {
+                city: '16',
+                district: 'Nilüfer',
+                neighborhood: 'Ertuğrul Mah',
+                addressInfo: 'No: 16, Dr: 7',
+            }
+        });
+        customerToken = generateToken(customerUser._id);
+
+        // create a test cook
+        cookUser = await User.create({
+            firstname: 'Test Cook',
+            lastname: 'User',
+            email: 'cook11@example.com',
+            password: 'testpassword',
+            phone: '5554567890',
+            address: {
+                city: '16',
+                district: 'Nilüfer',
+                neighborhood: 'Ertuğrul Mah',
+                addressInfo: 'No: 16, Dr: 7',
+            }
+        });
+        cookUser.role = 'cook'
+        await cookUser.save();
+
+        cookToken = generateToken(cookUser._id);
+
+        //create a test dish
+        testDish = await Dish.create({
+            name: "Test Dish",
+            cook: cookUser._id,
+            description: "This dish is for testing purposes only",
+            images: ["dishImage.jpg"],
+            quantity: 100,
+            price: 10,
+            category: "dinner"
+        });
+    });
+
+    afterAll(async () => {
+        // Clean up test data from the test database
+        await User.findOneAndDelete({ email: customerUser.email });
+        await User.findOneAndDelete({ email: cookUser.email });
+        await Dish.findOneAndDelete({ _id: testDish._id });
+    });
 
     describe('POST /api/cart', () => {
         it('adds dish to cart', async () => {
             const newItem = {
-                dishID,
+                dishID: testDish._id.toString(),
                 quantity: 2
             };
 
             const response = await request(app)
                 .post('/api/cart')
-                .set('Authorization', `Bearer ${token}`)
+                .set('Authorization', `Bearer ${customerToken}`)
                 .send(newItem);
 
             expect(response.statusCode).toBe(StatusCodes.OK);
@@ -29,7 +90,7 @@ describe('Cart routes', () => {
         it('gets cart for customer', async () => {
             const response = await request(app)
                 .get('/api/cart')
-                .set('Authorization', `Bearer ${token}`);
+                .set('Authorization', `Bearer ${customerToken}`);
 
             expect(response.statusCode).toBe(StatusCodes.OK);
             expect(response.body.data.cartItems).toEqual(expect.any(Array));
@@ -41,24 +102,24 @@ describe('Cart routes', () => {
             const quantity = 3;
 
             const response = await request(app)
-                .put(`/api/cart/${dishID}`)
-                .set('Authorization', `Bearer ${token}`)
+                .put(`/api/cart/${testDish._id.toString()}`)
+                .set('Authorization', `Bearer ${customerToken}`)
                 .send({ quantity });
 
             expect(response.statusCode).toBe(StatusCodes.OK);
-            expect(response.body.data.cartItems).toEqual(expect.arrayContaining([expect.objectContaining({ product: dishID, quantity })]));
+            expect(response.body.data.cartItems).toEqual(expect.arrayContaining([expect.objectContaining({ product: testDish._id.toString(), quantity })]));
         });
     });
 
     describe('DELETE /api/cart/{dishID}', () => {
         it('deletes a dish from cart', async () => {
             const response = await request(app)
-                .delete(`/api/cart/${dishID}`)
-                .set('Authorization', `Bearer ${token}`)
+                .delete(`/api/cart/${testDish._id.toString()}`)
+                .set('Authorization', `Bearer ${customerToken}`)
                 .send();
 
             expect(response.statusCode).toBe(StatusCodes.OK);
-            expect(response.body.data.cartItems.find(item => item.id === dishID)).toBeFalsy();
+            expect(response.body.data.cartItems.find(item => item.id === testDish._id.toString())).toBeFalsy();
         });
     });
 
@@ -66,7 +127,7 @@ describe('Cart routes', () => {
         it('deletes customer\'s cart', async () => {
             const response = await request(app)
                 .delete(`/api/cart`)
-                .set('Authorization', `Bearer ${token}`)
+                .set('Authorization', `Bearer ${customerToken}`)
                 .send();
 
             expect(response.statusCode).toBe(StatusCodes.NO_CONTENT); // best if there is response like message 'deleted successfully'
@@ -77,7 +138,7 @@ describe('Cart routes', () => {
         it('It returns 404 (NOT_FOUND) if the customer don\'t have a cart', async () => {
             const response = await request(app)
                 .get('/api/cart')
-                .set('Authorization', `Bearer ${token}`);
+                .set('Authorization', `Bearer ${customerToken}`);
 
             expect(response.statusCode).toBe(StatusCodes.NOT_FOUND);
         });
